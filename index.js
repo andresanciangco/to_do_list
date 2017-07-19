290752cf7836ceeca7a8af7bfa395dae2db8b7a6
Andre Sanciangco <jadsanciangco@gmail.com>
2017-07-19 15:30:40 +0000

    message

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#add-new-todo-button').click(function(){
+    var todoBlock=$(this).closest('#new-todo-block');
+    var description=todoBlock.find('#new-todo-description');
+    var pomodoroEstimate=todoBlock.find('#new-pomodoro-estimate');
+    $("ul").append("<li><input-type='checkbox'/>"+description.val()+"<spanclass=pomodoro-estimate">"+pomodoroEstimate.val()+" pomodoros</span></li>")
+    })
